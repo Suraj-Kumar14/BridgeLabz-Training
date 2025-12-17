ca2f215b554514a1c2a4b40d60d29d302f50003e
Suraj Kumar <suraj1122003@gmail.com>
2025-12-17 12:16:29 +0530

    [Suraj] : Added Age Of Harry

---
+++ gcr-codebase/programming-elements/Level 1/HarryAge.java
@@ -0,0 +1,9 @@
+class AgeOfHarry
+{
+     public static void main(String [] args)
+	 {
+	     int harryAge,birthYear=2000;
+		 int currentYear=harryAge-birthYear;
+		 System.out.println("Harry's age in 2024 is "+currentYear);
+    }
+}
