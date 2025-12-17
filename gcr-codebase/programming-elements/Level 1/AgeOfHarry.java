c9f7366db0fa1914042eb5d8af0ee256ff7ea477
Suraj Kumar <suraj1122003@gmail.com>
2025-12-17 15:14:05 +0530

    [Suraj] : Updated Age of Harry

--- gcr-codebase/programming-elements/Level 1/AgeOfHarry.java
+++ gcr-codebase/programming-elements/Level 1/AgeOfHarry.java
@@ -6,10 +6,13 @@ public class AgeOfHarry
 	     //Create two int variable harryAge, birthYear and assign the value 2000
 	     int harryAge,birthYear=2000;
 		 
-		 //Create a int variable currentYear to indicate the age difference between the harry current age and birthYear
-		 int currentYear=harryAge-birthYear;
+	     //Create a int variable currentYear and assign the value 2024
+	     int currentYear=2024;
+
+	     // Calculate the age of Harry by substracting birthyear from currentyear
+	      harryAge= currentYear-birthYear;
 		 
-		 //Displaying Current year  as output.
-		 System.out.println("Harry's age in 2024 is "+currentYear);
+	     //Displaying Current year  as output.
+	     System.out.println("Harry's age in 2024 is "+harryAge);
     }
 } 
