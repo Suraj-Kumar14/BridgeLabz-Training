b616bb451f98e23c748e8f729836e89604d6616e
Suraj Kumar <suraj1122003@gmail.com>
2025-12-17 14:28:23 +0530

    [Suraj] : Added Update comments

---
+++ gcr-codebase/programming-elements/Level 1/AgeOfHarry.java
@@ -0,0 +1,15 @@
+//Creating Class with name AgeOfHarry to indicating the purpose is to display The Current Year/Age of Harry
+public class AgeOfHarry
+{
+     public static void main(String [] args)
+	 { 
+	     //Create two int variable harryAge, birthYear and assign the value 2000
+	     int harryAge,birthYear=2000;
+		 
+		 //Create a int variable currentYear to indicate the age difference between the harry current age and birthYear
+		 int currentYear=harryAge-birthYear;
+		 
+		 //Displaying Current year  as output
+		 System.out.println("Harry's age in 2024 is "+currentYear);
+    }
+} 
