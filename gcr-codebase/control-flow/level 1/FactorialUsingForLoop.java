77d8eec4ec074f429a289d5a7fa236db69a8499d
Suraj Kumar <suraj1122003@gmail.com>
2025-12-18 22:11:38 +0530

    [Suraj] : Added Factorial using for loop.

---
+++ gcr-codebase/control-flow/level 1/FactorialUsingForLoop.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+//Create a class name FactorialUsingForLoop to indicating the purpose of find the factorial using for loop. 
+public class FactorialUsingForLoop
+{
+	public static void main(String [] args)
+	{
+	   //Create Scanner object for taking input from the user.
+	     Scanner sc =new Scanner(System.in);
+	   //Declare variable factorial and initialize to 1, and also declare a another variable number.
+		 int factorial=1, number;
+	   // Taking input from the user
+	     System.out.print("Enter number: ");
+		 number=sc.nextInt();
+		 
+	   //Check user entered positive number
+	     if(number<0)
+		 {
+		   System.out.println("Please enter a valid no!");
+		   return;
+		 }
+	   //Calculating Factorial
+		 for(factorial=1;number>0;number--)
+		 {
+		   factorial*=number;
+		  }
+		  System.out.println("Factorial is "+factorial);
+		  
+		 sc.close();
+	}
+}
