a50aec42463e5ff1dd96d99f393befafc1a24445
Suraj Kumar <suraj1122003@gmail.com>
2025-12-18 23:18:08 +0530

    [Suraj] : Added FizzBuzz solution.

---
+++ gcr-codebase/control-flow/level 2/FizzBuzz.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+//Create a class name FizzBuzz to indicating the purpose to print multiple of 3 print Fizz instead of number and multiple of 5 print FizzBuzz. 
+public class FizzBuzz
+{
+	public static void main(String [] args)
+	{
+	   //Create Scanner object for taking input from the user.
+	     Scanner sc =new Scanner(System.in);
+	   //Declare a int variable number and take input from user.	   
+		 System.out.print("Enter number: ");
+		 int number=sc.nextInt();
+	   //Check for positive integer
+		 if(number<1)
+		 {
+		   System.out.println("Please enter a positive number!");
+		   return;
+		 }
+		 
+	   //Iterate loop and display result
+	     for(int i=1;i<=number;i++)
+		 {
+		   if (i%3==0 && i%5==0)
+		   {
+		   System.out.println("FizzBuzz");
+		   }
+		   else if (i%3==0)
+		   {
+		   System.out.println("Fizz");
+		   }
+		   else if (i%5==0)
+		   {
+		   System.out.println("Buzz");
+		   }
+		   else 
+		   {
+		   System.out.println(i);
+		   }
+		 }
+		 sc.close();
+	}
+}
