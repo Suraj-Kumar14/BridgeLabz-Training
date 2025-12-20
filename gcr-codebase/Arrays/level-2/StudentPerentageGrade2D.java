e5e65f8462feb86d5def96e21b978e3487d0a9c5
Suraj Kumar <suraj1122003@gmail.com>
2025-12-21 00:21:35 +0530

    [Suraj]: Added Percentage,grade file using 2D array

---
+++ gcr-codebase/Arrays/level-2/StudentPerentageGrade2D.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+//Create a class StudentPerentageGrade2D to calculate the percentage, grade of three marks
+public class StudentPerentageGrade2D
+{
+	public static void main(String []args)
+	{
+		//Create a Scanner object
+		Scanner sc=new Scanner(System.in);
+		//take input to number of students
+		System.out.print("Enter number of student: ");
+		int number=sc.nextInt();
+		//Array to store marks of physics,chemistry, and maths
+		int [][]marks=new int[number][3];
+		double []percentage=new double[number];
+		char []grade=new char[number];
+		
+		//Input marks of each student
+		for(int i=0;i<number;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+			    do {
+				    if(j==0)
+					    System.out.print("Enter Physics marks of "+(i+1)+" student: ");
+					else if(j==1)
+					    System.out.print("Enter chemistry marks of "+(i+1)+" student: ");
+					else 
+						System.out.print("Enter maths marks of "+(i+1)+" student: ");
+						
+					marks[i][j]=sc.nextInt();					
+					if(marks[i][j]<0)
+					{
+					  System.out.println("Marks cannot be negative. Enter again.");			
+					}				
+				}while(marks[i][j]<0);
+			}
+			//Calculate percentage	
+			percentage[i]=(marks[i][0]+marks[i][1]+marks[i][2])/3.0; 
+			//Calculate Grade
+			if(percentage[i]>=80){
+				grade[i]='A';
+			}
+			else if(percentage[i]>=70&&percentage[i]<=79){
+				grade[i]='B';
+			}
+			else if(percentage[i]>=60&&percentage[i]<=69){
+				grade[i]='C';
+			}
+			else if(percentage[i]>=50&&percentage[i]<=59){
+				grade[i]='D';
+			}
+			else if(percentage[i]>=40&&percentage[i]<=49){
+				grade[i]='E';
+			}
+			else {
+				grade[i]='R';
+			}
+		}
+		System.out.println();
+		//Display marks, percentage and grade of a student
+		for(int i=0;i<number;i++){
+			System.out.print("The Marks, Percentage, and Grade of "+(i+1)+" Student are:"+"\n");
+			System.out.print("Marks: "+(marks[i][0]+marks[i][1]+marks[i][2])+"\n");
+			System.out.print("Percentage: "+percentage[i]+"\n");
+			System.out.print("Grade: "+grade[i]+"\n");
+			System.out.println();
+		}
+		sc.close();
+	}
+}
