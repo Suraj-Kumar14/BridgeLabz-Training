147d33ae5624ca298616cb904a098449172e9064
Suraj Kumar <suraj1122003@gmail.com>
2025-12-24 13:15:46 +0530

    [Suraj]: Added DateFormating file

---
+++ gcr-codebase/Extras/Built-In-Function/DateFormatting.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+//creating class DateFormatting to compute the date format
+public class DateFormatting{
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+
+		//Date formating
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+
+       //Displaying output
+        System.out.println("Format 1 (dd/MM/yyyy): " + today.format(format1));
+        System.out.println("Format 2 (yyyy-MM-dd): " + today.format(format2));
+        System.out.println("Format 3 (EEE, MMM dd, yyyy): " + today.format(format3));
+    }
+}
