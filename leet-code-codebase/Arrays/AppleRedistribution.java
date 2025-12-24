0cdd9d7d4e2ee569d8385f338f9b054d4132281a
Suraj Kumar <suraj1122003@gmail.com>
2025-12-24 12:14:34 +0530

    [Suraj]: Added AppleRedistribution file

---
+++ leet-code-codebase/Arrays/AppleRedistribution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        Arrays.sort(capacity);
+        int sum=0;
+       for(int a:apple){
+        sum+=a;
+       }
+       int count=0;
+       for(int i=capacity.length-1;i>=0;i--){
+        sum-=capacity[i];
+        count++;
+        if(sum<=0){
+            break;
+        }
+       }
+       return count;
+    }
+}
