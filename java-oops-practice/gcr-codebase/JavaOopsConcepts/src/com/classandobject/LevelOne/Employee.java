3c2966f4401d25b1c6695d6562fd3311742344cf
Suraj Kumar <suraj1122003@gmail.com>
2025-12-26 17:14:31 +0530

    [Suraj]: Added Employee Details

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/LevelOne/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/LevelOne/Employee.java
@@ -1,10 +1,30 @@
 package com.classandobject.LevelOne;
 
 public class Employee {
-
+	
+	//non-static variables
+	private String name;
+	private int id;
+	private double salary;
+	
+	//Constructor
+	public Employee(String name,int id,double salary){
+		this.name=name;
+		this.id=id;
+		this.salary=salary;
+	}
+	
+	//Creating method to display employee details
+	public void displayEmployee() {
+		System.out.println("Employee Name: "+name);
+		System.out.println("Employee id: "+id);
+		System.out.println("Employee Salary: "+salary);
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello suraj");
+		Employee emp=new Employee("Rohan",1,50000);//calling constructor
+		emp.displayEmployee();//calling the display method
 	}
 
 }
