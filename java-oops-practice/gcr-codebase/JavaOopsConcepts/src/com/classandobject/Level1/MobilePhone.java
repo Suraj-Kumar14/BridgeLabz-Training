e1ed5855b465ad81b52fba1bcbff01f58c826b3e
Suraj Kumar <suraj1122003@gmail.com>
2025-12-26 21:34:38 +0530

    [SURAJ]: Added MobilePhone file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/Level1/MobilePhone.java
@@ -0,0 +1,33 @@
+package com.classandobject.Level1;
+
+public class MobilePhone {
+	//Declaring variable for entering the details
+	String brand;
+	String model;
+	double price;
+	
+	//Constructor
+	MobilePhone(String brand,String model,double price){
+		this.brand=brand;
+		this.model=model;
+		this.price=price;
+	}
+	
+	//method to display the mobile phone details
+	public void displayMobileDetails() {
+		System.out.println("Brand of mobile: "+brand);
+		System.out.println("Model of mobile: "+model);
+		System.out.println("Price of mobile: "+price);
+		System.out.println("-----------------");
+	}
+	
+	public static void main(String[] args) {
+		MobilePhone mobile1=new MobilePhone("VIVO","VIVO V29",15999);
+		MobilePhone mobile2=new MobilePhone("ONE PLUS","ONE PLUS nord4",39999);
+		MobilePhone mobile3=new MobilePhone("APPLE","iphone pro16",79999);
+		
+		mobile1.displayMobileDetails();
+		mobile2.displayMobileDetails();
+		mobile3.displayMobileDetails();
+	}
+}
