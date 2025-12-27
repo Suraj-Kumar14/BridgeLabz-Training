f049157b241bed897ac6c14785aadd3d6818a744
Suraj Kumar <suraj1122003@gmail.com>
2025-12-27 22:48:14 +0530

    [Suraj]: Added PostgraduateStudent file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/Level1/PostgraduateStudent.java
@@ -0,0 +1,19 @@
+package com.constructor.Level1;
+
+public class PostgraduateStudent extends Student {
+	
+	//subclass constructor
+	public PostgraduateStudent(int rollNumber,String name,double CGPA) {
+		super(rollNumber,name,CGPA);
+	}
+	
+	//Method demonstrating access to protected member
+	public void displayDetails() {
+		//public
+		System.out.println("Roll Number: "+rollNumber); 
+		//protected
+		System.out.println("Name: "+name);
+		//private via public method
+		System.out.println("CGPA: "+getCGPA());
+	}
+}
