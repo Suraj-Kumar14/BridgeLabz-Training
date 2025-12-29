6080fecd6e065f49ae8f308224a3dbf557d69630
Suraj Kumar <suraj1122003@gmail.com>
2025-12-29 23:57:19 +0530

    [Suraj]: Added EmployeeMain file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/keywordandoperator/Level1/EmployeeMain.java
@@ -0,0 +1,20 @@
+package com.keywordandoperator.Level1;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  // Set the company name (static variable)
+	       Employee.setCompanyName("Tech Solutions Inc.");
+	       // Create new employee instances
+	       Employee emp1 = new Employee("Thamarai", 101, "Software Engineer");
+	       Employee emp2 = new Employee("Rohan", 102, "Project Manager");
+	       // Display total number of employees
+	       Employee.displayTotalEmployees();
+	       // Display employee details
+	       emp1.displayEmployeeDetails();
+	       emp2.displayEmployeeDetails();
+
+	}
+
+}
