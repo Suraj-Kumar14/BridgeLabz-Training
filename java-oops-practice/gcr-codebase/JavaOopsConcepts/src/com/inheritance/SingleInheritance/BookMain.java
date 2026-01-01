e851798c745b453db750a305ca6156c3fc4b2d62
Suraj Kumar <suraj1122003@gmail.com>
2026-01-01 12:56:55 +0530

    [Suraj]: Added BookMain file of Book super class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/SingleInheritance/BookMain.java
@@ -0,0 +1,9 @@
+package com.inheritance.SingleInheritance;
+
+public class BookMain {
+		public static void main(String [] args) {
+			Book author=new Author("Wings of Fire",2000,"A.P.J. Abdul Kalam","Missile man of india.");
+			
+			author.displayInfo();
+		}
+}
