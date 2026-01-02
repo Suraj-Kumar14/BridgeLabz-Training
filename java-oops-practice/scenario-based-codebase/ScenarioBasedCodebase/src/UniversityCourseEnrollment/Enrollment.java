464134a7bb5f86450bf867618ffed1048401196e
Suraj Kumar <suraj1122003@gmail.com>
2026-01-03 00:05:30 +0530

    [Suraj]: Added Enrollment childclass file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/UniversityCourseEnrollment/Enrollment.java
@@ -0,0 +1,17 @@
+package UniversityCourseEnrollment;
+
+public class Enrollment {
+	private Student student;
+    private Course course;
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public void showDetails() {
+        student.displayInfo();
+        course.displayCourse();
+        System.out.println("----------------------");
+    }
+}
