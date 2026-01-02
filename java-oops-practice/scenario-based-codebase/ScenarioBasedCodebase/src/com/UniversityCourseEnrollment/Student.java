d31a8bf2b4b8bc30a96952aab3ecebfa5430e9e6
Suraj Kumar <suraj1122003@gmail.com>
2026-01-03 00:18:53 +0530

    [Suraj]: Added abstract Student file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/UniversityCourseEnrollment/Student.java
@@ -0,0 +1,36 @@
+package com.UniversityCourseEnrollment;
+
+public abstract class Student {
+	public  String name;
+	public  int studentId;
+	private double gpa;
+	
+	//without elective preference
+	public Student(int studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+        this.gpa = 0.0;
+    }
+	
+	//with elective preference
+	 public Student(int studentId, String name, double gpa) {
+        this.studentId = studentId;
+	    this.name = name;
+	    this.gpa = gpa;
+    }
+		
+	protected void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getTranscript() {
+        return "ID: " + studentId + ", Name: " + name + ", GPA: " + gpa;
+    }
+
+    public abstract void displayInfo();
+	
+}
