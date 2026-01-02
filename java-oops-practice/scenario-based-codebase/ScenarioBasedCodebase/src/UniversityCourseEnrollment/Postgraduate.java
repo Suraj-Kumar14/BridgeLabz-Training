43adc0f5eeeaca87b31c910baaff39c1d6693ed1
Suraj Kumar <suraj1122003@gmail.com>
2026-01-03 00:03:50 +0530

    [Suraj]: Added Postgraduate childclass file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/UniversityCourseEnrollment/Postgraduate.java
@@ -0,0 +1,22 @@
+package UniversityCourseEnrollment;
+
+public class Postgraduate extends Student implements Graded {
+
+    public Postgraduate(int id, String name, double gpa) {
+        super(id, name, gpa);
+    }
+
+    // Pass / Fail grading
+    @Override
+    public void assignGrade(double marks) {
+        if (marks >= 50)
+            setGpa(3.5);
+        else
+            setGpa(0.0);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Postgraduate → " + getTranscript());
+    }
+}
