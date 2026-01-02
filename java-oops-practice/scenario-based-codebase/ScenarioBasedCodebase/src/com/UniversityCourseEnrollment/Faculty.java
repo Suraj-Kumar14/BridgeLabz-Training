4b27844953302a0b971504eb83dbcbd21bdb198a
Suraj Kumar <suraj1122003@gmail.com>
2026-01-03 00:22:16 +0530

    [Suraj]: Added Faculty childclass file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/UniversityCourseEnrollment/Faculty.java
@@ -0,0 +1,14 @@
+package com.UniversityCourseEnrollment;
+
+public class Faculty {
+
+    private String facultyName;
+
+    public Faculty(String name) {
+        this.facultyName = name;
+    }
+
+    public void assignGrade(Graded student, double marks) {
+        student.assignGrade(marks);
+    }
+}
