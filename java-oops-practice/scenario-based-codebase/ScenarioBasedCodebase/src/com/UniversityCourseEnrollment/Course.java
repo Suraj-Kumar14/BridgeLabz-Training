6bc8b74ab40c66568fcaa48be62404f6c4ef77dc
Suraj Kumar <suraj1122003@gmail.com>
2026-01-03 00:20:45 +0530

    [Suraj]: Added Course childclass file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/UniversityCourseEnrollment/Course.java
@@ -0,0 +1,17 @@
+package com.UniversityCourseEnrollment;
+
+public class Course {
+
+    private String courseCode;
+    private String courseName;
+
+    public Course(String code, String name) {
+        this.courseCode = code;
+        this.courseName = name;
+    }
+
+    public void displayCourse() {
+        System.out.println("Course: " + courseCode + " - " + courseName);
+    }
+
+}
