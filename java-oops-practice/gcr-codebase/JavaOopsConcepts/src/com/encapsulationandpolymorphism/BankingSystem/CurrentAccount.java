e517f5d8cfc7e493a8daa977697ec0e86257c910
Suraj Kumar <suraj1122003@gmail.com>
2026-01-03 17:48:35 +0530

    [Suraj]: Added CurrentAccount class file of Banking System

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/BankingSystem/CurrentAccount.java
@@ -0,0 +1,25 @@
+package com.encapsulationandpolymorphism.BankingSystem;
+
+public class CurrentAccount extends BankAccount implements Loanable {
+
+    private static final double INTEREST_RATE = 0.02; 
+
+    public CurrentAccount(String accNo, String name, double balance) {
+        super(accNo, name, balance);
+    }
+
+    @Override
+    public double calculateInterest() {
+        return balance * INTEREST_RATE;
+    }
+
+    @Override
+    public void applyForLoan(double amount) {
+        System.out.println("Current Account loan applied for: " + amount);
+    }
+
+    @Override
+    public double calculateLoanEligibility() {
+        return balance * 10;
+    }
+}
