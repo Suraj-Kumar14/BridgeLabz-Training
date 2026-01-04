40051d485b0e3e15f862f6b10bd8145036aa3963
Suraj Kumar <suraj1122003@gmail.com>
2026-01-05 01:27:54 +0530

    [Suraj]: Added Main class of StudentRecord file

--- dsa-practice/gcr-code-base/DataStructure/src/com/linkedlist/singlylinkedlist/studentrecordmanagement/Main.java
+++ dsa-practice/gcr-code-base/DataStructure/src/com/linkedlist/singlylinkedlist/studentrecordmanagement/Main.java
@@ -2,4 +2,19 @@ package com.linkedlist.singlylinkedlist.studentrecordmanagement;
 
 public class Main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList l1 = new LinkedList();
+		l1.addBeginning( 101,"Arjun", 22, 'A');
+		l1.addEnd(103,"Ram", 23, 'B');
+		l1.addAtPosition( 102,"Kishan", 24, 'C', 2);
+		
+		l1.searchByRoll(102);
+		l1.updateGrade(102, 'D');
+		l1.viewStudent();
+		
+		l1.deleteStduent(102);
+		l1.viewStudent();
+	}
+
 }
