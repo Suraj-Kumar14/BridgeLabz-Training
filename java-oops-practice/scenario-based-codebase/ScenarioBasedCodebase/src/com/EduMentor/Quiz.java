716465ab43a014ee4c5dd2b6397d1c246c44cdca
Suraj Kumar <suraj1122003@gmail.com>
2026-01-06 22:04:57 +0530

    [Suraj]: Added Quiz class file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/EduMentor/Quiz.java
@@ -0,0 +1,50 @@
+package com.EduMentor;
+import java.util.*;
+
+public class Quiz {
+
+	   
+    private List<String> questions;
+
+   
+    private final List<String> correctAnswers;
+
+    private int score;
+    private String difficulty;
+
+  
+    public Quiz(List<String> questions, List<String> correctAnswers) {
+        this(questions, correctAnswers, "Medium");
+    }
+
+    public Quiz(List<String> questions, List<String> correctAnswers, String difficulty) {
+        this.questions = questions;
+        this.correctAnswers = correctAnswers;
+        this.difficulty = difficulty;
+        this.score = 0;
+    }
+
+  
+    public List<String> getQuestions() {
+        return questions;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+  
+    public void evaluate(List<String> userAnswers) {
+        for (int i = 0; i < correctAnswers.size(); i++) {
+            if (userAnswers.get(i).equals(correctAnswers.get(i))) {
+                score++; 
+            }
+        }
+    }
+
+   
+    public double calculatePercentage() {
+        return (score * 100.0) / correctAnswers.size();
+    }
+}
+
