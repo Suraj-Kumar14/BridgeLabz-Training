fdf64c20abd382789f9b74481b0f972d702c5f12
Suraj Kumar <suraj1122003@gmail.com>
2026-01-06 22:02:08 +0530

    [Suraj]: Added Edumentor main file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/EduMentor/EduMentor.java
@@ -0,0 +1,29 @@
+package com.EduMentor;
+
+import java.util.Arrays;
+
+public class EduMentor {
+
+    public static void main(String[] args) {
+
+        Learner learner1 = new Learner(
+                "suraj",
+                "suraj@gmail.com",
+                101,
+                true);
+
+        learner1.showRole();
+
+        Quiz quiz = new Quiz(
+                Arrays.asList("Java is OOP?", "Java supports inheritance?"),
+                Arrays.asList("Yes", "Yes"),
+                "Easy");
+
+        quiz.evaluate(Arrays.asList("Yes", "Yes"));
+
+        System.out.println("Score: " + quiz.getScore());
+        System.out.println("Percentage: " + quiz.calculatePercentage());
+
+        learner1.generateCertificate(); 
+    }
+}
