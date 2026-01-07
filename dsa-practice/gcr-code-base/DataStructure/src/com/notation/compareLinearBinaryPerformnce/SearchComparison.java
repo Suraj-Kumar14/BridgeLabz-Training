801779a0bcc12cd8992fe8f3c7744eefad1cd6e5
Suraj Kumar <suraj1122003@gmail.com>
2026-01-08 00:09:58 +0530

    [Suraj]: Added SearchComparison file

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/notation/compareLinearBinaryPerformnce/SearchComparison.java
@@ -0,0 +1,35 @@
+package com.notation.compareLinearBinaryPerformnce;
+
+public class SearchComparison {
+	
+	//Linear Search O(N)
+	public static int linearSearch(int[] arr, int target) {
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==target) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	//Binary Search O(log N)
+	public static int binarySearch(int[] arr, int target) {
+		int left=0; 
+		int right=arr.length-1;
+		while(left<=right) {
+			int mid=(left+right)/2;
+			
+			if(arr[mid]==target) {
+				return mid;
+			}
+			else if(arr[mid]<target) {
+				left=mid+1;
+			}
+			else {
+				right=mid-1;
+			}
+		}
+		return -1;
+	}
+		
+}
