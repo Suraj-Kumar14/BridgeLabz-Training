5d7fc061c4d0e0f68446c70df2e972a62ef103ae
Suraj Kumar <suraj1122003@gmail.com>
2026-01-08 21:44:37 +0530

    [Suraj]: Added Hotel class file of TourMate

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/TourMate/Hotel.java
@@ -0,0 +1,23 @@
+package com.TourMate;
+
+public class Hotel implements IBookable{
+	private double cost;
+	
+	public Hotel(double cost) {
+		this.cost=cost;
+	}
+	
+	public double getCost() {
+		return cost;
+	}
+	
+	@Override
+	public void book() {
+		System.out.println("Hotel Booked!");
+	}
+	
+	@Override
+	public void cancel() {
+		System.out.println("Hotel cancelled!");
+	}
+}
