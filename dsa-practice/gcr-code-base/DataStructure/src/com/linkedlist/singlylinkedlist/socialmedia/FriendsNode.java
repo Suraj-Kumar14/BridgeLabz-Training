e014d5b15c30710bf6bbef193f4119a314100cdb
Suraj Kumar <suraj1122003@gmail.com>
2026-01-08 11:26:42 +0530

    [Suraj]: Added FriendsNode file

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/linkedlist/singlylinkedlist/socialmedia/FriendsNode.java
@@ -0,0 +1,11 @@
+package com.linkedlist.singlylinkedlist.socialmedia;
+
+public class FriendsNode {
+	public int friendID;
+	FriendsNode next;
+	
+	public FriendsNode(int friendID) {
+		this.friendID = friendID;
+		this.next = null;
+	}
+}
