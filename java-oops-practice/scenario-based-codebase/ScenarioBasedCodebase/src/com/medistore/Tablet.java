acc5e9219af0335b3d6814537e5749c81933b20c
Suraj Kumar <suraj1122003@gmail.com>
2026-01-08 20:06:51 +0530

    [Suraj]: Added Tablet class of medistore file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/medistore/Tablet.java
@@ -0,0 +1,43 @@
+package com.medistore;
+
+import java.time.LocalDate;
+
+public class Tablet extends Medicine{
+	
+	public Tablet(String name,double price,LocalDate expiryDate,int quantity) {
+		super(name,price,expiryDate,quantity);
+	}
+	
+	@Override
+	public void sell(int quantity) {
+		if((!checkExpiry(LocalDate.now())) && (getQuantity()>quantity)) {
+			System.out.println("\n----------Bill----------");
+			System.out.println("Name         :"+getName());
+			System.out.println("Price:       :"+getPrice());
+			System.out.println("Total Amount :"+getPrice()*quantity);
+			System.out.println("Expiry date :"+getExpiryDate());
+			
+			setQuantity(getQuantity()-quantity);
+			
+		}
+		else {
+			System.out.println("Medicine not available");
+		}
+			
+		System.out.println();
+	}
+	
+	@Override
+	public boolean checkExpiry(LocalDate date) {
+		return date.isAfter(getExpiryDate())||date.isEqual(getExpiryDate());
+	}
+	
+	@Override
+	public void details() {
+		System.out.println("\n--------Medicine Details--------");
+		System.out.println("Name of medicine:"+getName());
+		System.out.println("Price           :"+getPrice());
+		System.out.println("Available       :"+getQuantity());
+		System.out.println("Expiry Date     :"+getExpiryDate());
+	}
+}
