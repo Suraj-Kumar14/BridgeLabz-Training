b54167ad72a7bbcccd78e1d73b5da0ff2f8982e8
Suraj Kumar <suraj1122003@gmail.com>
2026-01-10 01:41:43 +0530

    [Suraj]: Added VegItem class file of FoodLoop

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/FoodLoop/VegItem.java
@@ -0,0 +1,11 @@
+package com.FoodLoop;
+
+
+public class VegItem extends FoodItem{
+
+	public VegItem(int foodId,String name, String category, double price, boolean availability, int stock) {
+		super(foodId,name, "VegItem", price, availability, stock);
+		
+	}
+
+}
