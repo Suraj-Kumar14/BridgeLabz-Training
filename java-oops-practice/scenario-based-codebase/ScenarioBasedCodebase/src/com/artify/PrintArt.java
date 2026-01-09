30412db4389073875964a40121e0cb758e5922ce
Suraj Kumar <suraj1122003@gmail.com>
2026-01-09 14:32:41 +0530

    [Suraj]: Added PrintArt class of Artify file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/artify/PrintArt.java
@@ -0,0 +1,10 @@
+package com.artify;
+
+public class PrintArt extends ArtWork{
+
+	public PrintArt(String title, String artist, double price, String licenseType) {
+		super(title, artist, price, licenseType,"Print Art");
+		
+	}
+
+}
