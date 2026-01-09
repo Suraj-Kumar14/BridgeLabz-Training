a9632578d4a8fe242f4c136aa739a93593ea83ce
Suraj Kumar <suraj1122003@gmail.com>
2026-01-09 14:30:42 +0530

    [Suraj]: Added DigitalArt class of Artify file

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedCodebase/src/com/artify/DigitalArt.java
@@ -0,0 +1,11 @@
+package com.artify;
+
+public class DigitalArt extends ArtWork{
+
+	public DigitalArt(String title, String artist, double price, String licenseType) {
+		super(title, artist, price, licenseType, "Digital Art");
+		
+	}
+
+		
+}
