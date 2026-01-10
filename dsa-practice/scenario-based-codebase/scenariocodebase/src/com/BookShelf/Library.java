c6cd32ab593434c4153888f880f8994aaeb3d0c2
Suraj Kumar <suraj1122003@gmail.com>
2026-01-10 16:36:31 +0530

    [Suraj]: Added Library class of BookShelf file

---
+++ dsa-practice/scenario-based-codebase/scenariocodebase/src/com/BookShelf/Library.java
@@ -0,0 +1,72 @@
+package com.BookShelf;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Library {
+	private HashMap<String,LinkedList<Book>>libraryCatalog;
+	
+	Library(){
+		libraryCatalog=new HashMap<>();
+	}
+	
+	//method addBook to genre
+	public void addBook(String genre,Book book) {
+		if(!libraryCatalog.containsKey(genre)) {
+			libraryCatalog.put(genre,new LinkedList<Book>());
+		}
+			libraryCatalog.get(genre).add(book);			
+			System.out.println(book+" added to  "+genre);	
+			System.out.println();
+			return;
+	}
+	
+	//Borrow book
+	public void borrowBook(String genre,String title) {
+		if(libraryCatalog.containsKey(genre)) {
+			LinkedList<Book>books=libraryCatalog.get(genre);
+			for(Book b: books) {
+				if(b.getTitle().equals(title) && !b.isBorrowed) {
+					b.isBorrowed=true;
+					books.remove(b);
+					System.out.println(title+" borrowed from "+genre);
+				    System.out.println();
+					return;
+				}
+			}
+			System.out.println(title+" not found in "+genre);
+			return ;
+		}
+		 System.out.println("Book not available to borrow: " + title);
+	}
+	
+	//Return book method
+	public void returnBook(String genre,String title) {
+		if (libraryCatalog.containsKey(genre)) {
+            for (Book b : libraryCatalog.get(genre)) {
+                if (b.getTitle().equals(title) && b.isBorrowed) {
+                    b.isBorrowed = false; 
+                    System.out.println(title + " returned to " + genre);
+                    return;
+                }
+            }
+        }
+        System.out.println("Book was not borrowed: " + title);
+        System.out.println();
+	}
+	
+	//Display catalog
+	public void displayCatalog() {
+		if(libraryCatalog.isEmpty()) {
+			System.out.println("No Genre available!");
+			return;
+		}
+		for(String genre:libraryCatalog.keySet()) {
+			System.out.println("Genre: "+genre);
+			for(Book book: libraryCatalog.get(genre)) {
+				System.out.println(" "+book);
+				System.out.println();
+			}
+		}
+	}
+}
