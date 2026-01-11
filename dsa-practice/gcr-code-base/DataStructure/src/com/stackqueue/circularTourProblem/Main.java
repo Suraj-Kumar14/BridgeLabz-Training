53c5ae53b17c551bc4c0c6d2ef5991fdc179f57b
Suraj Kumar <suraj1122003@gmail.com>
2026-01-11 17:15:28 +0530

    [Suraj]: Added main class file

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/stackqueue/circularTourProblem/Main.java
@@ -0,0 +1,19 @@
+package com.stackqueue.circularTourProblem;
+
+public class Main {
+	
+	public static void main(String[] args){
+		int[] petrol= {4,6,7,4};
+		int[] distance= {6,5,3,5};
+		
+		int start=CircularTour.startIndex(petrol,distance);
+		if(start!=-1) {
+			System.out.println("Start at pump index: "+start);
+		}
+		else {
+			System.out.println("No solution possible");
+		}
+			
+	}
+
+}
