91a54cd337489ad665a0bdec3b972169f7a5cb0a
Suraj Kumar <suraj1122003@gmail.com>
2026-01-11 19:15:00 +0530

    [Suraj]: Added RotationPointSearch file using binary search

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/searchingalgorithms/binarysearch/RotationPointSearch.java
@@ -0,0 +1,30 @@
+package com.searchingalgorithms.binarysearch;
+
+public class RotationPointSearch {
+
+	public static int findRotationPoint(int[] arr) {
+		int left=0;
+		int right=arr.length-1;
+		
+		while(left<right) {
+			int mid=left+(right-left)/2;
+		
+			if(arr[mid]>arr[right]) {
+				left=mid+1;
+			}else {
+				right=mid;
+			}
+		}	
+		return left;
+	}
+	
+	public static void main(String[] args) {
+		int [] arr= {15,18,2,3,6,12};
+		
+		int rotationIndex=findRotationPoint(arr);
+		
+		System.out.println("Rotation point index: "+rotationIndex);
+		System.out.println("Smallest element: "+arr[rotationIndex]);
+	}
+
+}
