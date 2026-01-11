0a2cecbbe6532ea3059d53d08addcabe9796c4ba
Suraj Kumar <suraj1122003@gmail.com>
2026-01-11 13:59:40 +0530

    [Suraj]: Added Node class of BrowserBuddy

---
+++ dsa-practice/scenario-based-codebase/scenariocodebase/src/com/BrowserBuddy/Node.java
@@ -0,0 +1,13 @@
+package com.BrowserBuddy;
+
+public class Node {
+	 String url;
+	 Node next,prev;
+	 
+	 public Node(String url) {
+		 this.url=url;
+		 this.next=null;
+		 this.prev=null;
+	 }
+	 
+}
