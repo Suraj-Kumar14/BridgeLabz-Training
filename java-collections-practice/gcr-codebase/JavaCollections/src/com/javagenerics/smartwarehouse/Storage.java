21c54764e4a012a0600dbbc95e6dea382241140c
Suraj Kumar <suraj1122003@gmail.com>
2026-01-17 11:28:54 +0530

    [Suraj]: Added Storage class file of SmartWareHouse

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Storage.java
@@ -0,0 +1,28 @@
+package com.javagenerics.smartwarehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage<T extends WarehouseItem> {
+	List<T>itemList=new ArrayList<>();
+	
+	//add item
+	public void addItem(T item) {
+		itemList.add(item);
+		System.out.println("Added item.");
+	}
+	
+	//get item
+	public List<T> getItemList(){
+		return itemList;
+	}
+	
+	//display the item list
+		public void showItem(List<? extends WarehouseItem> item) {
+			for(WarehouseItem items : item) {
+				System.out.println("\nItem Name: "+items.getItemName());
+				System.out.println("Item Price: "+items.getPrice());
+				System.out.println("Item Quantity: "+items.getQuantity());
+			}
+		}
+	}
