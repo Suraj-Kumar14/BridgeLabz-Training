0e7a09a71c3c62cce9d73f0da0908c6ca92f7acc
Suraj Kumar <suraj1122003@gmail.com>
2026-01-17 11:28:28 +0530

    [Suraj]: Added Electronics class file of SmartWareHouse

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Electronics.java
@@ -0,0 +1,10 @@
+package com.javagenerics.smartwarehouse;
+
+public class Electronics extends WarehouseItem{
+
+	public Electronics(String itemName, double price, int quantity) {
+		super(itemName, price, quantity);
+		
+	}
+
+}
