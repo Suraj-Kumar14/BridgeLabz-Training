956b5aae2a4b2e89fb4578cb610e2ea61b872356
Suraj Kumar <suraj1122003@gmail.com>
2026-01-17 11:30:42 +0530

    [Suraj]: Added Groceries class file of SmartWareHouse

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Groceries.java
@@ -0,0 +1,10 @@
+package com.javagenerics.smartwarehouse;
+
+public class Groceries extends WarehouseItem{
+
+	public Groceries(String itemName, double price, int quantity) {
+		super(itemName, price, quantity);
+	
+	}
+
+}
