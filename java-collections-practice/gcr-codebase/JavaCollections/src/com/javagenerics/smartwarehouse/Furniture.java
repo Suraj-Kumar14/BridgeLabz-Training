f14ba28f41882e33bb46e332c073abe1da15c598
Suraj Kumar <suraj1122003@gmail.com>
2026-01-17 11:30:21 +0530

    [Suraj]: Added Furniture class file of SmartWareHouse

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Furniture.java
@@ -0,0 +1,10 @@
+package com.javagenerics.smartwarehouse;
+
+public class Furniture extends WarehouseItem{
+
+	public Furniture(String itemName, double price, int quantity) {
+		super(itemName, price, quantity);
+		
+	}
+
+}
