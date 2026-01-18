12074dfa4732bd8e21cc679266e9e7c2ce279860
Suraj Kumar <suraj1122003@gmail.com>
2026-01-18 23:43:58 +0530

    [Suraj]: Added Node class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/collections/list/reverselist/reverseusinglinkedlist/Node.java
@@ -0,0 +1,12 @@
+package com.collections.list.reverselist.reverseusinglinkedlist;
+
+class Node {
+    int data;
+    Node next;
+    
+   public  Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
