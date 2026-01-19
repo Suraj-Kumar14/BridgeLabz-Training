afbe6e13999503f3ba6bcf047e92020779ba9538
Suraj Kumar <suraj1122003@gmail.com>
2026-01-19 12:20:28 +0530

    [Suraj]: Added check subset of another set

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/collections/set/Subsets.java
@@ -0,0 +1,24 @@
+package com.collections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Subsets {
+
+	public static void main(String[] args) {
+		Set<Integer>set1=new HashSet<>();
+		set1.add(2);
+		set1.add(3);
+		
+		Set<Integer>set2=new HashSet<>();
+		set2.add(1);
+		set2.add(2);
+		set2.add(3);
+		set2.add(4);
+		
+		boolean isSubset=set2.containsAll(set1);
+		System.out.println(isSubset);
+		
+	}
+
+}
