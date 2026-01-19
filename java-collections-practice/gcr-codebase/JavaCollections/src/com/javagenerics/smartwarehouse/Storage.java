5e5b59cc5379ccc9c7e6a537e52da4476269700f
Suraj Kumar <suraj1122003@gmail.com>
2026-01-20 00:21:39 +0530

    [Suraj]: modify Storage class file of Warehouse

--- java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Storage.java
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Storage.java
@@ -18,7 +18,7 @@ public class Storage<T extends WarehouseItem> {
 	}
 	
 	//display the item list
-		public void showItem(List<? extends WarehouseItem> item) {
+		public static void showItem(List<? extends WarehouseItem> item) {
 			for(WarehouseItem items : item) {
 				System.out.println("\nItem Name: "+items.getItemName());
 				System.out.println("Item Price: "+items.getPrice());
