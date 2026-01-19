96162dcd2502c977d00bd3a097c51016bf062a4e
Suraj Kumar <suraj1122003@gmail.com>
2026-01-20 01:49:58 +0530

    [Suraj]: Added Course class file of CourseManagement System

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/coursemanagement/Course.java
@@ -0,0 +1,32 @@
+package com.javagenerics.coursemanagement;
+
+public class Course<T extends CourseType> {
+	private String courseName;
+	private String department;
+	private T courseType;
+	
+	public Course(String name, String department, T courseType) {
+		this.courseName = name;
+		this.department = department;
+		this.courseType = courseType;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public T getCourseType() {
+		return courseType;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [courseName=" + courseName + ", department=" + department + ", courseType=" + courseType.getType() +", evaluation="+courseType.getEvaluationMethod()+ "]";
+	}
+	
+	
+}
