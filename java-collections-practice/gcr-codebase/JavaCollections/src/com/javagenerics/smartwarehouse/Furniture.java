a783eecd82ab8db284796e2465802b55ea9ac186
Suraj Kumar <suraj1122003@gmail.com>
2026-01-20 00:21:13 +0530

    [Suraj]: modify Furniture class file of Warehouse

--- java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Furniture.java
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/smartwarehouse/Furniture.java
@@ -3,8 +3,11 @@ package com.javagenerics.smartwarehouse;
 public class Furniture extends WarehouseItem{
 
 	public Furniture(String itemName, double price, int quantity) {
-		super(itemName, price, quantity);
-		
+		super(itemName, price, quantity);		
 	}
 
+	@Override
+	public String toString() {
+		return getItemName()+": Rs"+getPrice();
+	}	
 }
