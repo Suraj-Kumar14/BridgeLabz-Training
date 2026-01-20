17b525326754ee3a14777d072ad4ecc75a32b3b9
Suraj Kumar <suraj1122003@gmail.com>
2026-01-21 01:09:00 +0530

    [Suraj]: Added Meal class file of MealPlanGenerator

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javagenerics/mealplangenerator/Meal.java
@@ -0,0 +1,25 @@
+package com.javagenerics.mealplangenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Meal<T extends MealPlan> {
+	private List<T>mealList=new ArrayList<>();
+	
+	//add meal
+	public void addMeal(T meal) {
+		mealList.add(meal);
+	}
+	
+	//view meal
+	public void viewMeal() {
+		for(T meal: mealList) {
+			meal.displayMeal();
+		}
+	}
+	
+	//get meal
+	public List<T>getMeal(){
+		return mealList;
+	}
+}
