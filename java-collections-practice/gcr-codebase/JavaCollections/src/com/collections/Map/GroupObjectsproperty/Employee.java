a0ffa7c4b48d68d034da42e61249600fd6c3b3a4
Suraj Kumar <suraj1122003@gmail.com>
2026-01-21 21:50:24 +0530

    [Suraj]: Added Employee class file of GroupObject

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/collections/Map/GroupObjectsproperty/Employee.java
@@ -0,0 +1,22 @@
+package com.collections.Map.GroupObjectsproperty;
+
+public class Employee {
+	private String name;
+	private String department;
+	
+	public Employee(String name, String department) {
+		this.name = name;
+		this.department = department;
+	}	
+	
+	public String getDepartment() {
+		return department;
+	}
+
+	@Override
+    public String toString() {
+        return name;
+    }
+	
+	
+}
