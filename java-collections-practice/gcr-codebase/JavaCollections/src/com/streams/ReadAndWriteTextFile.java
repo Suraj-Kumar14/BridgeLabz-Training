6cf95184f40bafed63fc897e606cf990c1c9e32d
Suraj Kumar <suraj1122003@gmail.com>
2026-01-24 13:06:39 +0530

    [Suraj]: Added ReadAndWrite class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/streams/ReadAndWriteTextFile.java
@@ -0,0 +1,38 @@
+package com.streams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.*;
+
+public class ReadAndWriteTextFile {
+
+	public static void main(String[] args) throws IOException {
+		
+		String text="C:\\Users\\suraj\\OneDrive\\Documents\\input.txt";
+		
+		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Name: ");
+		String name=reader.readLine();
+		
+		System.out.print("Enter Age: ");
+		int age=Integer.parseInt(reader.readLine());
+		
+		System.out.print("Enter college: ");
+		String college=reader.readLine();
+		
+		FileOutputStream filepath = new FileOutputStream("textFiles/text.txt");
+		
+		try (BufferedWriter w = new BufferedWriter(new OutputStreamWriter(filepath))){
+			w.write(name);
+			w.write(String.valueOf("\n"));
+			w.write(String.valueOf(age));
+			w.write(String.valueOf("\n"));
+			w.write(college);
+			System.out.println("Added file data.");
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
+	}
+
+}
