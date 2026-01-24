d7cd2bac0a902f30a87b940955f2660789e6105c
Suraj Kumar <suraj1122003@gmail.com>
2026-01-25 00:36:23 +0530

    [Suraj]: Added ByteArrayStream class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/streams/ImageToByteArray.java
@@ -0,0 +1,29 @@
+package com.streams;
+
+import java.io.*;
+
+public class ImageToByteArray {
+		
+	public static void main(String[] args) throws Exception{
+		try
+		{
+			FileInputStream file = new FileInputStream("textFiles/firstImage.jpg");
+				
+			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+				
+			int b;
+			while((b=file.read())!=-1) {
+				bytes.write(b);
+			}
+				
+			byte[] arrayByte = bytes.toByteArray();
+				
+			FileOutputStream result = new FileOutputStream("textFiles/copyImage.jpg");
+			result.write(arrayByte);
+			
+		}catch(Exception e) {
+			System.out.println("File not found!");
+		}
+	}
+
+}
