fc17ca9a85bb1c7fd45d234695baa982e57d2612
Suraj Kumar <suraj1122003@gmail.com>
2026-01-24 15:56:19 +0530

    [Suraj]: Added InvalidAgeException class file of customException

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exception/customexception/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package com.exception.customexception;
+
+@SuppressWarnings("serial")
+public class InvalidAgeException extends Exception{
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
