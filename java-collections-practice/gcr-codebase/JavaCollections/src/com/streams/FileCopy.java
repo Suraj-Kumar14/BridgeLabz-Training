4d219d88978c4642360ee47530513ac3538a37fa
Suraj Kumar <suraj1122003@gmail.com>
2026-01-24 14:31:04 +0530

    [Suraj]: Added fileCopy class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/streams/FileCopy.java
@@ -0,0 +1,23 @@
+package com.streams;
+
+import java.io.*;
+
+
+public class FileCopy {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		try(BufferedWriter w=new BufferedWriter(new OutputStreamWriter(new FileOutputStream("textFiles/newbigText.txt")))){
+			String line;
+			BufferedReader read=new BufferedReader(new InputStreamReader(new FileInputStream("textFiles/bigText.txt")));
+			while((line=read.readLine())!=null) {
+				w.write(line);
+			}
+			read.close();
+			System.out.println("copy success.");
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found!");
+		}	
+	}
+}
