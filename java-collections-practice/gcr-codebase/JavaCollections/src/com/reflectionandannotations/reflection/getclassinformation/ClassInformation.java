c9a7104917fc8bd345b01a30b9202565e284e14d
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 03:12:16 +0530

    [Suraj]: Added classInformation class file of reflection

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/reflection/getclassinformation/ClassInformation.java
@@ -0,0 +1,46 @@
+package com.reflectionandannotations.reflection.getclassinformation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ClassInformation {
+
+	public static void main(String[] args) {
+		Person person=new Person("Arjun",30);
+		String className="com.reflectionandannotations.reflection.getclassinformation.Person";
+		
+		try {
+			
+			//1.load class
+			Class<?>cls=Class.forName(className);
+			System.out.println("\nClass: "+cls.getName());
+			
+			//2. constructor
+			System.out.println("\nConstructor: ");
+			Constructor<?>[]cons=cls.getDeclaredConstructors();
+			for (Constructor<?> constructor : cons) {
+                System.out.println("  " + constructor);
+            }
+
+			//3. field
+			System.out.println("\nFields: ");
+			Field[] fields=cls.getDeclaredFields();
+			for(Field field:fields) {
+				System.out.println(" "+field);
+			}
+			
+			//4. Method
+			System.out.println("\nMethod: ");
+			Method[] methods=cls.getDeclaredMethods();
+			for(Method method: methods) {
+				System.out.println(" "+method);
+			}
+			
+		}catch(ClassNotFoundException e) {
+			System.out.println("Class not found: "+className);
+		}
+
+	}
+
+}
