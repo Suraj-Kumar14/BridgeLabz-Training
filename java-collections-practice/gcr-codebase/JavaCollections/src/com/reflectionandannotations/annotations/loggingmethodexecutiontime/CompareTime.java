9e382a163ed8a52a759ce918a17c12c1f57eaf4e
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 21:03:49 +0530

    [Suraj]: Added CompareTime class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/annotations/loggingmethodexecutiontime/CompareTime.java
@@ -0,0 +1,17 @@
+package com.reflectionandannotations.annotations.loggingmethodexecutiontime;
+
+public class CompareTime {
+	
+	
+	@LogExecutionTime
+	public void beforeExecution() {
+	    
+	}
+	
+	@LogExecutionTime
+	public void AfterExecution() {
+		
+	}
+	
+	
+}
