aa18432fe2edeb613af22fc618608538cf66e469
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 13:12:37 +0530

    [Suraj]: Added Main class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/annotations/overridecorrectly/Main.java
@@ -0,0 +1,10 @@
+package com.reflectionandannotations.annotations.overridecorrectly;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Animal a=new Dog();
+		
+		a.makeSound();
+	}
+}
