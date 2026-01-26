634a85b746d1b77c34973fc9f073042d408cdb36
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 12:10:59 +0530

    [Suraj]: Added Author class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/reflection/retrieveannotations/Author.java
@@ -0,0 +1,12 @@
+package com.reflectionandannotations.reflection.retrieveannotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface Author {
+	String name();
+}
