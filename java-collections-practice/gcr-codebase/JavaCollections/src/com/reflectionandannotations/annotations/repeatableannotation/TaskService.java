edf310d8f6e66324921a8d45363350b0d498c059
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 20:28:49 +0530

    [Suraj]: Added TaskService class file

--- java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/annotations/repeatableannotation/TaskService.java
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/annotations/repeatableannotation/TaskService.java
@@ -1,8 +1,8 @@
 package com.reflectionandannotations.annotations.repeatableannotation;
 
 public class TaskService {
-	 @BugReport(description = "NullPointerException when input is null")
-	 @BugReport(description = "Slow performance for large data")
+//	 @BugReport(description = "NullPointerException when input is null")
+	// @BugReport(description = "Slow performance for large data")
 	 public void processTask() {
 	        System.out.println("Processing task...");
 	    }
