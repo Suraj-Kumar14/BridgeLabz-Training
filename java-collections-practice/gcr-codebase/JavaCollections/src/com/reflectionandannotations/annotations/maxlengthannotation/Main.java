4693997c23bfbfed355d75c03fc03ee81895f5e3
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 21:12:01 +0530

    [Suraj]: Added main class file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/annotations/maxlengthannotation/Main.java
@@ -0,0 +1,8 @@
+package com.reflectionandannotations.annotations.maxlengthannotation;
+
+public class Main {
+	public static void main(String[] args) {
+        new User("Suraj");          
+        new User("VeryLongUsername");
+    }
+}
