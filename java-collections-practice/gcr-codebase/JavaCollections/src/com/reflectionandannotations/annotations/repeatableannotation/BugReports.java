b6cd37976cd54f28bf8334963960f3cb1d4514bf
Suraj Kumar <suraj1122003@gmail.com>
2026-01-26 20:12:26 +0530

    [Suraj]: Added BugReports annotation file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/reflectionandannotations/annotations/repeatableannotation/BugReports.java
@@ -0,0 +1,12 @@
+package com.reflectionandannotations.annotations.repeatableannotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+ @interface BugReports {
+    BugReport[] value();
+}
