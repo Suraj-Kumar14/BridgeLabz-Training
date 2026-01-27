a7c95c4e1abc1d0862480284d74be5569c9d49a6
Suraj Kumar <suraj1122003@gmail.com>
2026-01-27 12:50:51 +0530

    [Suraj]: Added WriteDataToCSV class file

--- java-iostreams-practice/gcr-codebase/IOconcepts/src/com/csvfile/WriteDataToCSVFile.java
+++ java-iostreams-practice/gcr-codebase/IOconcepts/src/com/csvfile/WriteDataToCSVFile.java
@@ -8,11 +8,11 @@ public class WriteDataToCSVFile {
 		
 		try(FileWriter w=new FileWriter("DataFiles/employees.csv")){
 			
-			w.append("101,Allu,25,25000\n");
-			w.append("102,Sonu,30,45000\n");
-			w.append("103,Monu,22,35000\n");
-			w.append("104,Bholu,28,32000\n");
-			w.append("105,Kali,31,55000\n");
+			w.append("101,Allu,IT,25000\n");
+			w.append("102,Sonu,HR,45000\n");
+			w.append("103,Monu,Finance,35000\n");
+			w.append("104,Bholu,Marketing,32000\n");
+			w.append("105,Kali,Sales,55000\n");
 			
 			System.out.println("CSV file write successfully!");					
 			
