f43fe638a46f381eb08716bfa476daeffbb84ede
Suraj Kumar <suraj1122003@gmail.com>
2026-01-27 21:32:18 +0530

    [Suraj]: Added Car class file

---
+++ java-iostreams-practice/gcr-codebase/IOconcepts/src/com/json/objectintojson/Car.java
@@ -0,0 +1,17 @@
+package com.json.objectintojson;
+
+public class Car {
+	
+	private String brand;
+	private String model;
+	private int year;
+	private double price;
+	
+	public Car(String brand, String model, int year, double price) {
+		this.brand = brand;
+		this.model = model;
+		this.year = year;
+		this.price = price;
+	}	
+	
+}
