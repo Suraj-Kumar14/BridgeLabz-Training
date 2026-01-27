397177fa5afc4a4391b41f77071ca39424571b61
Suraj Kumar <suraj1122003@gmail.com>
2026-01-27 12:45:09 +0530

    [Suraj]: Added FilterRecords from csv file

--- java-iostreams-practice/gcr-codebase/IOconcepts/src/com/csvfile/FilterRecords.java
+++ java-iostreams-practice/gcr-codebase/IOconcepts/src/com/csvfile/FilterRecords.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadAndCountRows {
+public class FilterRecords {
 
 	public static void main(String[] args) {
 		try(BufferedReader br=new BufferedReader(new FileReader("DataFiles/students.csv"))){
