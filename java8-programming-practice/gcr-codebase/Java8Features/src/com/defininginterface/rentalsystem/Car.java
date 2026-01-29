ae3b62283f41f74510c57d028471528e2a0f483f
Suraj Kumar <suraj1122003@gmail.com>
2026-01-30 01:32:15 +0530

    [Suraj]: Added Car class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/defininginterface/rentalsystem/Car.java
@@ -0,0 +1,17 @@
+package com.defininginterface.rentalsystem;
+
+public class Car implements Vehicle{
+
+	@Override
+	public void rent() {
+		System.out.println("Car rented successfully");
+		
+	}
+
+	@Override
+	public void returnVehicle() {
+		System.out.println("Car returned successfully");
+		
+	}
+
+}
