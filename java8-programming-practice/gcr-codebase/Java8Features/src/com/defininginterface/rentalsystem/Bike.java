3dfba422ef789a800ea12fb000f914cb62fdea10
Suraj Kumar <suraj1122003@gmail.com>
2026-01-30 01:32:48 +0530

    [Suraj]: Added Bike class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/defininginterface/rentalsystem/Bike.java
@@ -0,0 +1,16 @@
+package com.defininginterface.rentalsystem;
+
+public class Bike implements Vehicle{
+	
+	@Override
+	public void rent() {
+		System.out.println("Bike rented successfully");
+		
+	}
+
+	@Override
+	public void returnVehicle() {
+		System.out.println("Bike returned successfully");
+		
+	}
+}
