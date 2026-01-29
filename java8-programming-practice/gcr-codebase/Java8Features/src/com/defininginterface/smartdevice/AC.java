de1f872e447ddc232432d92eec6017db6c69a14d
Suraj Kumar <suraj1122003@gmail.com>
2026-01-30 02:17:58 +0530

    [Suraj]: Added AC class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/defininginterface/smartdevice/AC.java
@@ -0,0 +1,15 @@
+package com.defininginterface.smartdevice;
+
+public class AC implements SmartDevice{
+
+	@Override
+	public void turnOn() {
+	System.out.println("AC is turn ON");		
+	}
+
+	@Override
+	public void turnOff() {
+	System.out.println("AC is turn OFF");		
+	}
+
+}
