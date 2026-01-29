0ac5f2e39ddeb70a7a87f4a58c23bb605bc1c21e
Suraj Kumar <suraj1122003@gmail.com>
2026-01-30 02:19:10 +0530

    [Suraj]: Added TV class file

--- java8-programming-practice/gcr-codebase/Java8Features/src/com/defininginterface/smartdevice/TV.java
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/defininginterface/smartdevice/TV.java
@@ -1,17 +1,15 @@
-package com.defininginterface.implementinginterface;
+package com.defininginterface.smartdevice;
 
 public class TV implements SmartDevice{
 
 	@Override
 	public void turnOn() {
-		System.out.println("TV is turn ON");
-		
+	System.out.println("TV is turn ON");		
 	}
 
 	@Override
 	public void turnOff() {
-		System.out.println("TV is turn OFF");
-		
+	System.out.println("TV is turn OFF");		
 	}
 
 }
