45b626ccfa61828dd71ab0787d3225b6d7ef762f
Suraj Kumar <suraj1122003@gmail.com>
2026-01-31 23:41:38 +0530

    [Suraj]: Added ProductInfo class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/interfacepractice/markerinterface/sensitivedatatagging/ProductInfo.java
@@ -0,0 +1,12 @@
+package com.interfacepractice.markerinterface.sensitivedatatagging;
+
+public class ProductInfo {
+	private String productName;
+	private double price;
+	
+	public ProductInfo(String productName,double price) {
+		this.productName=productName;
+		this.price=price;
+	}
+	
+}
