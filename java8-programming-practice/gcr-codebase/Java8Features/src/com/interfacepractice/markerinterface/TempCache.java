94ee006c6e4dd2e862c6db3b9f1d5e5c829b993d
Suraj Kumar <suraj1122003@gmail.com>
2026-01-31 22:24:44 +0530

    [Suraj]: Added TempCache class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/interfacepractice/markerinterface/TempCache.java
@@ -0,0 +1,10 @@
+package com.interfacepractice.markerinterface;
+
+public class TempCache {
+	
+	private String sessionId;
+	
+	public TempCache(String sessionId) {
+		this.sessionId=sessionId;
+	}
+}
