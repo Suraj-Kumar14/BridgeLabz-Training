49cbdacfec91a53006db394062246864940775b6
Suraj Kumar <suraj1122003@gmail.com>
2026-02-01 23:42:15 +0530

    [Suraj]: Added Stock class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/streamAPI/stockpricelogger/Stock.java
@@ -0,0 +1,26 @@
+package com.streamAPI.stockpricelogger;
+
+public class Stock {
+	private String name;
+	private double price;
+	
+	public Stock(String name,double price) {
+		this.name=name;
+		this.price=price;
+	}
+	
+//	public String getName() {
+//		return name;
+//	}
+//	
+//	public double getPrice() {
+//		return price;
+//	}
+
+	@Override
+	public String toString() {
+		return "Stock [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+}
