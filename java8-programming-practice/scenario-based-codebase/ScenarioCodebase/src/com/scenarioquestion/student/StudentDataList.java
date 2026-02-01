087981e166a51cdf70315adae0a9bb5c3c9098ee
Suraj Kumar <suraj1122003@gmail.com>
2026-02-01 18:13:34 +0530

    [Suraj]: Added StudentDataList class file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioCodebase/src/com/scenarioquestion/student/StudentDataList.java
@@ -0,0 +1,14 @@
+package com.scenarioquestion.student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDataList {
+	
+	List<Student>students=new ArrayList<>();
+	
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+	
+}
