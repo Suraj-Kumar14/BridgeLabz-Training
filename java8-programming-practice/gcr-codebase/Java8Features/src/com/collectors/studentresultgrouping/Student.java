79ed6f797a0c35ae332e863e4c6b08d00dafb29c
Suraj Kumar <suraj1122003@gmail.com>
2026-02-02 01:01:42 +0530

    [Suraj]: Added Student class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/collectors/studentresultgrouping/Student.java
@@ -0,0 +1,26 @@
+package com.collectors.studentresultgrouping;
+
+public class Student {
+	private String name;
+	private String grade;
+	
+	public Student(String name,String grade) {
+		this.name=name;
+		this.grade=grade;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getGrade() {
+		return grade;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", grade=" + grade + "]";
+	}
+	
+	
+}
