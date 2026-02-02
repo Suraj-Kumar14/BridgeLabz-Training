4d08337f5656e7d615a7f9ea4bbc8cc66ad214ee
Suraj Kumar <suraj1122003@gmail.com>
2026-02-02 22:54:57 +0530

    [Suraj]: Added Employee class file of employee_OfficeSystem

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioCodebase/src/com/scenarioquestion/employee_officesystem/Employee.java
@@ -0,0 +1,49 @@
+package com.scenarioquestion.employee_officesystem;
+
+public class Employee {
+	private String name;
+	private int age;
+	private String gender;
+	private String department;
+	private int yearOfJoining;
+	private double salary;
+	
+	public Employee(String name, int age, String gender, String department, int yearOfJoining, double salary) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + ", gender=" + gender + ", department="
+				+ department + ", yearOfJoining=" + yearOfJoining + ", salary=" + salary + "]";
+	}
+}
