7e17a991c95989e473fbb234cabe68487fdcf6f5
Suraj Kumar <suraj1122003@gmail.com>
2026-02-02 16:22:54 +0530

    [Suraj]: Added Student class file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioCodebase/src/com/scenarioquestion/studentschoolsystem/Student.java
@@ -0,0 +1,38 @@
+package com.scenarioquestion.studentschoolsystem;
+
+public class Student {
+	private String name;
+	private int age;
+	private int rank;
+	private int marks;
+	
+	public Student(String name,int age,int rank,int marks) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+		this.marks=marks;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+	
+	public int getMarks() {
+		return marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", rank=" + rank + ", marks=" + marks + "]";
+	}
+	
+}
