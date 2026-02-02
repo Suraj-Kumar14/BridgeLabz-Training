1f9ed63c02a92a4bb6a0c049c347220484180fbb
Suraj Kumar <suraj1122003@gmail.com>
2026-02-02 12:17:51 +0530

    [Suraj]: Added Employee class file

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/collectors/employeesalarycategorization/Employee.java
@@ -0,0 +1,31 @@
+package com.collectors.employeesalarycategorization;
+
+public class Employee {
+	private String name;
+	private String department;
+	private double salary;
+	
+	public Employee(String name,String department, double salary) {
+		super();
+		this.name = name;
+		this.department=department;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
