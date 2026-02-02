796af2e3312767e2d2d0ff924288d2d5c8e108cc
Suraj Kumar <suraj1122003@gmail.com>
2026-02-02 13:31:44 +0530

    [Suraj]: Added TransportService interface file of SmartCityTransport

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/workshoponjava8features/smartcitytransport/TransportService.java
@@ -0,0 +1,10 @@
+package com.workshoponjava8features.smartcitytransport;
+
+public interface TransportService {
+	
+	String getServicename();
+	String getRoute();
+	double getFare();
+	int getDepartureTime(); 
+	
+}
