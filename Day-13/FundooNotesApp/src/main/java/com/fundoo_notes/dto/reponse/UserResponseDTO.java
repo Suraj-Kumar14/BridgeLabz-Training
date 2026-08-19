dbaf007970a7b2a69f6f1103fe5a168dbd6cbf72
Suraj Kumar <suraj1122003@gmail.com>
2026-08-19 14:30:19 +0530

    [Suraj]: Add phone field in request and response dto

--- Day-13/FundooNotesApp/src/main/java/com/fundoo_notes/dto/reponse/UserResponseDTO.java
+++ Day-13/FundooNotesApp/src/main/java/com/fundoo_notes/dto/reponse/UserResponseDTO.java
@@ -15,4 +15,5 @@ public class UserResponseDTO {
 	private Long id;
 	private String name;
 	private String email;
+	private String phone;
 }
--- Day-13/FundooNotesApp/src/main/java/com/fundoo_notes/dto/request/RegisterRequestDTO.java
+++ Day-13/FundooNotesApp/src/main/java/com/fundoo_notes/dto/request/RegisterRequestDTO.java
@@ -2,6 +2,7 @@ package com.fundoo_notes.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +18,12 @@ public class RegisterRequestDTO {
 	@Email(message = "Invalid email format")
 	private String email;
 	
+	@NotBlank(message = "Phone number required")
+	@Pattern(regexp ="^[6-9]\\d{9}$", message="Enter a valid 10 digit number")
+	private String phone;
+	
 	@NotBlank(message = "Password is required")
 	@Size(min=8, message ="Password must contain at least 8 characters")
 	private String password;
+	
 }
